1f5a7c92db264e438bd5bcf6a1de5d616966e4be
Snarkface <tomtomkan@gmail.com>
2018-02-03 22:11:57 +0200

    Create a Broadcast Receiver for Wi-Fi P2P Intents

---
+++ WiFi P2P.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Net.Wifi.P2p;
+
+namespace Mobile_Adhoc_Triangulator
+{
+    /**
+    * A BroadcastReceiver that notifies of important Wi-Fi p2p events.
+    */
+    public class WiFiDirectBroadcastReceiver : BroadcastReceiver
+    {
+
+    public override void OnReceive(Context context, Intent intent)
+    {
+        String action = intent.Action;
+
+        if (WifiP2pManager.WifiP2pConnectionChangedAction.Equals(action))
+        {
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
+        }
+        else if (WifiP2pManager.WifiP2pPeersChangedAction.Equals(action))
+        {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        }
+        else if (WifiP2pManager.WifiP2pConnectionChangedAction.Equals(action))
+        {
+            // Respond to new connection or disconnections
+        }
+        else if (WifiP2pManager.WifiP2pThisDeviceChangedAction.Equals(action))
+        {
+            // Respond to this device's wifi state changing
+        }
+    }
+}
+
+
+}
