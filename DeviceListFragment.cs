ffd9e5693c1a3b0c90cca0df6a0f1ea7a46c0c93
Snarkface <tomtomkan@gmail.com>
2018-03-02 19:44:52 +0200

    Almost finish DeviceListFragment

--- DeviceListFragment.cs
+++ DeviceListFragment.cs
@@ -18,8 +18,8 @@ namespace Mobile_Adhoc_Triangulator
     public class DeviceListFragment : ListFragment, WifiP2pManager.IPeerListListener
     {
         private List<WifiP2pDevice> peers = new List<WifiP2pDevice>();
-        ProgressDialog progressDialog = null;
-        View mContentView = null;
+        private ProgressDialog progressDialog = null;
+        private View mContentView = null;
         private WifiP2pDevice device;
 
         public override void OnActivityCreated(Bundle savedInstanceState)
@@ -31,7 +31,7 @@ namespace Mobile_Adhoc_Triangulator
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             //mContentView = inflater.Inflate(R.layout.device_list, null);
-            return mContentView;
+            return this.mContentView;
         }
 
         /**
@@ -39,7 +39,7 @@ namespace Mobile_Adhoc_Triangulator
          */
         public WifiP2pDevice GetDevice()
         {
-            return device;
+            return this.device;
         }
 
         private static String GetDeviceStatus(WifiP2pDeviceState deviceStatus)
@@ -61,122 +61,131 @@ namespace Mobile_Adhoc_Triangulator
                     return "Unknown";
             }
         }
-        /**
+
+        /*
          * Initiate a connection with the peer.
          */
-        @Override
-        public void onListItemClick(ListView l, View v, int position, long id)
+        public override void OnListItemClick(ListView l, View v, int position, long id)
         {
-            WifiP2pDevice device = (WifiP2pDevice)getListAdapter().getItem(position);
-            ((DeviceActionListener)getActivity()).showDetails(device);
+            WifiP2pDevice device = (WifiP2pDevice)this.ListAdapter.GetItem(position);
+            ((IDeviceActionListener)this.Activity).ShowDetails(device);
         }
 
-        /**
+        /*
          * Array adapter for ListFragment that maintains WifiP2pDevice list.
          */
-        private class WiFiPeerListAdapter extends ArrayAdapter<WifiP2pDevice> {
+        private class WiFiPeerListAdapter : ArrayAdapter<WifiP2pDevice>
+        {
             private List<WifiP2pDevice> items;
-        /**
-         * @param context
-         * @param textViewResourceId
-         * @param objects
-         */
-        @Override
-            public View getView(int position, View convertView, ViewGroup parent)
-        {
-            View v = convertView;
-            if (v == null)
-            {
-                LayoutInflater vi = (LayoutInflater)getActivity().getSystemService(
-                        Context.LAYOUT_INFLATER_SERVICE);
-                v = vi.inflate(R.layout.row_devices, null);
-            }
-            WifiP2pDevice device = items.get(position);
-            if (device != null)
-            {
-                TextView top = (TextView)v.findViewById(R.id.device_name);
-                TextView bottom = (TextView)v.findViewById(R.id.device_details);
-                if (top != null)
-                {
-                    top.setText(device.deviceName);
-                }
-                if (bottom != null)
-                {
-                    bottom.setText(getDeviceStatus(device.status));
-                }
-            }
-            return v;
-        }
-    }
-    /**
-     * Update UI for this device.
-     * 
-     * @param device WifiP2pDevice object
-     */
-    public void updateThisDevice(WifiP2pDevice device)
-    {
-        this.device = device;
-        TextView view = (TextView)mContentView.findViewById(R.id.my_name);
-        view.setText(device.deviceName);
-        view = (TextView)mContentView.findViewById(R.id.my_status);
-        view.setText(getDeviceStatus(device.status));
-    }
+            Activity Activity;
 
-    public void onPeersAvailable(WifiP2pDeviceList peerList)
-    {
-        if (progressDialog != null && progressDialog.isShowing())
-        {
-            progressDialog.dismiss();
+            /*
+             * @param context
+             * @param textViewResourceId
+             * @param objects
+             */
+            public WiFiPeerListAdapter(Context context, int textViewResourceId,
+                            List<WifiP2pDevice> objects, Activity Activity) : base(context, textViewResourceId, objects)
+            {
+                this.items = objects;
+                this.Activity = Activity;
+            }
+
+            public override View GetView(int position, View convertView, ViewGroup parent)
+            {
+                View v = convertView;
+                if (v == null)
+                {
+                    LayoutInflater vi = (LayoutInflater)this.Activity.GetSystemService(
+                            Context.LayoutInflaterService);
+                    //v = vi.Inflate(R.layout.row_devices, null);
+                }
+                WifiP2pDevice device = items.ElementAt(position);
+                /*if (device != null)
+                {
+                    TextView top = (TextView)v.FindViewById(R.id.device_name);
+                    TextView bottom = (TextView)v.FindViewById(R.id.device_details);
+                    if (top != null)
+                    {
+                        top.SetText(device.DeviceName);
+                    }
+                    if (bottom != null)
+                    {
+                        bottom.SetText(GetDeviceStatus(device.Status));
+                    }
+                }*/
+                return v;
+            }
         }
-        peers.clear();
-        peers.addAll(peerList.getDeviceList());
-        ((WiFiPeerListAdapter)getListAdapter()).notifyDataSetChanged();
-        if (peers.size() == 0)
+
+        /**
+         * Update UI for this device.
+         * 
+         * @param device WifiP2pDevice object
+         */
+        public void UpdateThisDevice(WifiP2pDevice device)
         {
-            Log.d(WiFiDirectActivity.TAG, "No devices found");
+            this.device = device;
+            //TextView view = (TextView)mContentView.FindViewById(R.id.my_name);
+            //view.SetText(device.DeviceName);
+            //view = (TextView)mContentView.FindViewById(R.id.my_status);
+            //view.SetText(GetDeviceStatus(device.Status));
+        }
+
+    public void OnPeersAvailable(WifiP2pDeviceList peerList)
+    {
+        if (progressDialog != null && progressDialog.IsShowing)
+        {
+            progressDialog.Dismiss();
+        }
+        peers.Clear();
+        peers.AddRange(peerList.DeviceList);
+        ((WiFiPeerListAdapter)ListAdapter).NotifyDataSetChanged();
+        if (peers.Count == 0)
+        {
+            Log.Debug(WiFiDirectActivity.TAG, "No devices found");
             return;
         }
     }
-    public void clearPeers()
+    public void ClearPeers()
     {
-        peers.clear();
-        ((WiFiPeerListAdapter)getListAdapter()).notifyDataSetChanged();
+        peers.Clear();
+        ((WiFiPeerListAdapter)ListAdapter).NotifyDataSetChanged();
     }
     /**
      * 
      */
     public void onInitiateDiscovery()
     {
-        if (progressDialog != null && progressDialog.isShowing())
+        if (progressDialog != null && progressDialog.IsShowing)
         {
-            progressDialog.dismiss();
+            progressDialog.Dismiss();
         }
-        progressDialog = ProgressDialog.show(getActivity(), "Press back to cancel", "finding peers", true,
-                true, new DialogInterface.OnCancelListener() {
-                        @Override
-                        public void onCancel(DialogInterface dialog)
+        progressDialog = ProgressDialog.Show(Activity, "Press back to cancel", "finding peers", true,
+            true, new DialogInterfaceOnCancelListener());
+    }
+
+        private class DialogInterfaceOnCancelListener : DialogInterface, IDialogInterfaceOnCancelListener
         {
+            public DialogInterfaceOnCancelListener() : base
+            {
+            }
 
+            public void OnCancel(IDialogInterface dialog)
+            {
+                //throw new NotImplementedException();
+            }
         }
-    }
 
-    /**
-     * An interface-callback for the activity to listen to fragment interaction
-     * events.
-     */
-    public interface DeviceActionListener
-    {
-        void showDetails(WifiP2pDevice device);
-        void cancelDisconnect();
-        void connect(WifiP2pConfig config);
-        void disconnect();
-    }
-
-    public WiFiPeerListAdapter(Context context, int textViewResourceId,
-                List<WifiP2pDevice> objects)
-    {
-        super(context, textViewResourceId, objects);
-        items = objects;
-    }
-
+        /**
+         * An interface-callback for the activity to listen to fragment interaction
+         * events.
+         */
+        public interface IDeviceActionListener
+        {
+            void ShowDetails(WifiP2pDevice device);
+            void SancelDisconnect();
+            void Connect(WifiP2pConfig config);
+            void Disconnect();
+        }
 }
